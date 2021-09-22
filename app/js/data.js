0c3a2da6d73259507600bc23a21d4160b02e5852
harisad <81264553+harisad@users.noreply.github.com>
2021-09-23 01:08:47 +0700

    update commit

---
+++ app/js/data.js
@@ -0,0 +1,41 @@
+$(function () {
+    EmployeeManager.start({
+        employees: [
+            {
+                id: 1,
+                name: 'Haris Mujianto',
+                nim : '1910511027',
+                tel: '082246812825',
+                email: 'harism@upnvj.ac.id',
+            },
+            {
+                id: 2,
+                name: 'Pandapotan Sitanggang',
+                nim : '1910511095',
+                tel: '085771909109',
+                email: 'panda@upnvj.ac.id',
+            },
+            {
+                id: 3,
+                name: 'Ikhlasul Amal',
+                nim : '1910511125',
+                tel: '081293311095',
+                email: 'ikhlasulamal@upnvj.ac.id',
+            },
+            {
+                id: 4,
+                name: 'Muhammad Ardiansyah',
+                nim : '1910511084',
+                tel: '087882168565',
+                email: 'ardiansyah@upnvj.ac.id',
+            },
+            {
+                id: 5,
+                name: 'Muhammad Riski Yusuf',
+                nim : '1910511127',
+                tel: '089636743554',
+                email: 'ardiansyah@upnvj.ac.id',
+            },
+        ],
+    });
+});
---
+++ app/js/models/employee.js
@@ -0,0 +1,8 @@
+EmployeeManager.Models.Employee = Backbone.Model.extend({
+  defaults: {
+    name: null,
+    nim : null,
+    tel: null,
+    email: null,
+  }
+});
